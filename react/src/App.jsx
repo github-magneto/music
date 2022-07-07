ee14991b29b213fe58a1e87db8e57addd5b75c15
magneto <1735721264@qq.com>
2022-07-07 12:42:51 +0800

    x

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
-import { HashRouter, Route, Switch } from 'react-router-dom'
 
 import Music from './view/Page.Music'
+import Base64 from './view/Page.Base64'
 import Message from './view/Component.Message'
 import Spin from './view/Component.Spin'
 
 function App() {
-  return <HashRouter>
+  return <>
     <Message />
     <Spin />
 
-    <Switch>
-      <Route path='/' exact><Music /></Route>
-      <Route path='/Base64' component={() => import('./view/Page.Base64')} exact />
-    </Switch>
-  </HashRouter>
+    <Music />
+  </>
 }
 
 export default App
